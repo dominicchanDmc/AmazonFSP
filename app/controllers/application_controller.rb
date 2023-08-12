51120980b9614ac7420dd9acec1e2b3e890e456c
dominicchanDmc <dominic@DominicInspiron>
2023-08-12 16:35:47 -0700

    build login backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,26 @@
 class ApplicationController < ActionController::API
     include ActionController::RequestForgeryProtection
-  
+
+    rescue_from StandardError, with: :unhandled_error
+    rescue_from ActionController::InvalidAuthenticityToken,
+      with: :invalid_authenticity_token
+
     protect_from_forgery with: :exception
     before_action :snake_case_params, :attach_authenticity_token
+
+    def test
+      if params.has_key?(:login)
+        login!(User.first)
+      elsif params.has_key?(:logout)
+        logout!
+      end
+    
+      if current_user
+        render json: { user: current_user.slice('id', 'username', 'session_token') }
+      else
+        render json: ['No current user']
+      end
+    end
 
     def current_user
       @current_user ||= User.find_by(session_token: session[:session_token])
@@ -39,4 +57,16 @@ class ApplicationController < ActionController::API
         status: :unprocessable_entity
     end
   
+    def unhandled_error(error)
+      if request.accepts.first.html?
+        raise error
+      else
+        @message = "#{error.class} - #{error.message}"
+        @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+        render 'api/errors/internal_server_error', status: :internal_server_error
+        
+        logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+      end
+    end
+
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ActiveRecord::Base.transaction do
+puts "Destroying tables..."
+User.destroy_all
+
+puts "Resetting primary keys..."
+ActiveRecord::Base.connection.reset_pk_sequence!('users')
+
+puts "Creating users..."
+mike = User.create!(username: 'mike', email: 'mike@google.com', password: 'password')
+paulo = User.create!(username: 'paulo', email: 'paulo@google.com', password: 'password')
+peter = User.create!(username: 'peter', email: 'peter@google.com', password: 'password')
+john = User.create!(username: 'john', email: 'john@google.com', password: 'password')
+mary = User.create!(username: 'mary', email: 'mary@google.com', password: 'password')
+joe = User.create!(username: 'joe', email: 'joe@google.com', password: 'password')
+peter = User.create!(username: 'peter', email: 'peter@google.com', password: 'password')
+peter = User.create!(username: 'peter', email: 'peter@google.com', password: 'password')
+
+puts "Seeding complete!"
+end

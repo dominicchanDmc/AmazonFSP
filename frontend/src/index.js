6090bc334210c4ed9a5b4d593c6fa243f4b4e714
dominicchanDmc <dominic@DominicInspiron>
2023-08-12 00:26:28 -0700

    start

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
   <React.StrictMode>

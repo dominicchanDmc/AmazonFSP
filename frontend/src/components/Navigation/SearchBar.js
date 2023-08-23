6a4115fff7d8a381310c2c3f96fefcb46c14b1e6
dominicchanDmc <dominic@DominicInspiron>
2023-08-23 10:54:42 -0700

    finish pulldown

--- frontend/src/components/Navigation/SearchBar.js
+++ frontend/src/components/Navigation/SearchBar.js
@@ -1,20 +1,30 @@
-import React, {useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
-// const categories =  [
-//     { name: "All", categories: "all" },
-//     { name: "Amazon Devices", categories: "aDevices" },
-//     { name: "Amazon Warehouse", categories: "amazonWarehouse" },
-//     { name: "Apps & Games", categories: "appsGames" }
-//   ]
+const categories =  [
+    { name: "All", categories: "all" },
+    { name: "Amazon Devices", categories: "aDevices" },
+    { name: "Amazon Warehouse", categories: "amazonWarehouse" },
+    { name: "Apps & Games", categories: "appsGames" }
+  ]
   function SearchBar(){
 
     const [searchInput, setSearchInput] = useState("");
+    const [selectedOption, setSelectedOption] = useState("All");
+    const selectRef = useRef(null); // Ref for the <select> element
 
     const handleChange = (e) => {
     e.preventDefault();
     setSearchInput(e.target.value);
     };
-    
+
+    useEffect(() => {
+        if (selectRef.current) {
+            const optionText = selectRef.current.options[selectRef.current.selectedIndex].text;
+            const optionWidth = optionText === 'All' ? '50px' : `${optionText.length * 10}px`;
+            selectRef.current.style.width = optionWidth;
+        }
+      }, [selectedOption]);
+
     // if (searchInput.length > 0) {
     // categories.filter((categories) => {
     //     return categories.name.match(searchInput);
@@ -24,15 +34,20 @@ import React, {useState} from 'react'
         <div id="nav-search">
             <form id="nav-search-bar-form">
                 <div className="nav-left">
-                    {/* <select
+                {/* <span class="nav-search-label">All</span>
+                  <i class="fa fa-caret-down" aria-hidden="true"></i> */}
+                    <select
                         name='categories'
-                        value={categories}
-                        className='nav-select'
+                        ref={selectRef}
+                        // value={categories}
+                        className='select-search'
+                        value={selectedOption}
+                        onChange={(e) => setSelectedOption(e.target.value)}
                     >
                         {categories.map(categories =>
-                        <option key={categories}>{categories}</option>
+                        <option key={categories.categories}>{categories.name}</option>
                         )}
-                    </select> */}
+                    </select>
                 </div>
                 <div className="nav-fill">
                     <input type="text"

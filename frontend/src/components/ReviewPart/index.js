24d90ceeff5e815490950119d7761793faf933e5
dominicchanDmc <dominic@DominicInspiron>
2023-08-24 18:10:55 -0700

    add button

--- frontend/src/components/ReviewPart/index.js
+++ frontend/src/components/ReviewPart/index.js
@@ -5,12 +5,20 @@ function ReviewPart(props) {
     const {reviewInfo,averageRating,totalRatingCount} = props;
     return (<>
             <div className="review-rating">
-                {totalRatingCount> 0 ? (
+                {totalRatingCount> 0 ? (<>
                     <div className="rating-box">
                         <RatingPart averageRating={averageRating} caller={"review"} />
                         <span>{totalRatingCount} <span>global ratings</span></span>
                         <div className="rating-number">{averageRating} out of 5 stars</div>
+                        <div className="breck"></div>
+                        <div className="review-card">
+                            <span className="price-fontSize-18"><strong>Review this product</strong></span>
+                            <span className="price-fontSize-14">Share your thoughts with other customers</span>
+                            <button className='review-card-button'>Write a customer review</button>
+                            <div className="breck"/>
+                        </div>
                     </div>
+                </>
                 ):null}            
             </div>
             <div className="review-review">

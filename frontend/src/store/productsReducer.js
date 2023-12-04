8b2441877b9d90d9784ed7ba89977e0002bac942
dominicchanDmc <dominicchan.dmc@gmail.com>
2023-12-04 11:54:08 -0800

    1

--- frontend/src/store/productsReducer.js
+++ frontend/src/store/productsReducer.js
@@ -76,7 +76,7 @@ const productReducer = (state = {}, action) => {
   // const nextState = { ...state }
 
   switch (action.type) { 
-    case RECEIVE_PRODUCT:
+    case RECEIVE_PRODUCT: 
       return {...state, ...action.product} 
     case RECEIVE_PRODUCTS: 
       // return Object.assign(nextState, action.products)
